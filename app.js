7e69adfcf23988a6300b9d48e01dd4cc7b6880b4
Mestiz4 <info.mestiz4@gmail.com>
2022-10-22 09:43:38 -0500

    entrega al fin

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ alert("por favor ingresa si o no")
 
 if (seleccion=="si"){
 alert ("A continuación verás nuestra lista de cursos")
-
 let todosLosCursos = cursos.map(
     (curso) => curso.nombre + " " + curso.precio + " $"
 );
@@ -33,7 +32,8 @@ while(seleccion != "no"){
     let producto = prompt ("Agrega un curso al carrito")
     let precio = 0
 
-    if(producto == "ingles" || producto == "frances" || producto == "portugues" || producto == "italiano" || producto == "mandarin" )
+    if(producto == "ingles" || producto == "frances" || producto == "portugues" || producto == "italiano" 
+    || producto == "mandarin" ){
     switch(producto){
         case "ingles":
         precio = 2000;
@@ -53,6 +53,7 @@ while(seleccion != "no"){
         default:
             break;
     }
+
     
             let unidades = parseInt (prompt ("Cuántos cursos desea inscribir?"))
     
@@ -70,60 +71,4 @@ while (seleccion == "no"){
     })
     break;
 }
-
-
-
-
-
-
-
-/*let cursoEscogido
-
-cursoEscogido = prompt ("Escriba el número del curso que desea cursar 1. Inglés $2000, 2. Francés $5000 3.Italiano $8000 4. Mandarin $8000 5.Japones $8000")
-
-if (cursoEscogido !="") {
-    alert("Gracias por escogernos, continue con su compra")
-
 }
-
-else{
-    alert("No debe haber campos vacíos")
-}
-
-
-
-
-function cargarItems(carrito)
-{
-    let articulo;
-    let seguir;
-    do{
-    articulo = cargarUnItem();
-    carrito.push(articulo);
-    seguir = prompt ("desea continuar comprando cursos? si/no")
-} while(seguir =='si')
-}
-
-function mostrarCarrito(carrito)
-{
-    for(let articulo of carrito)
-    {
-        console.log("Nombre: " + cursos.precio + "\n"
-                    + "Precio: " + cursos.sabor + "\n")
-}
-}
-
-//sumar precios del carrito
-total = carrito.reduce((suma, articulo) => {
-    return suma + articulo.precio
-}, 0)
-
-
-const carrito = []
-cargarItems(carrito);
-console.log(carrito);
-mostrarCarrito(carrito);*/
-
-
-
-
