40c8de4bc4da6240c174131d4c16f8eb1dbfd869
Mestiz4 <info.mestiz4@gmail.com>
2022-10-22 01:28:18 -0500

    añadi prompts y alerts

--- app.js
+++ app.js
@@ -1,18 +1,66 @@
-class Pulpas {
-    constructor (precio,sabor,tamaño){
-    this.precio = parseFloat(precio)
-    this.sabor = sabor
-    this.tamaño = tamaño
+//carrito
+
+class cursos {
+    constructor (nombre, precio)
+    {
+    this.nombre = nombre;
+    this.precio = precio;
     }
 }
 
-let Pulpa1 = new Pulpas('2000', 'fresa', '120 oz')
-let Pulpa2 = new Pulpas('5000', 'guanabana', '120 oz')
-let Pulpa3 = new Pulpas('7000', 'maracuya', '120 oz') 
+let cursoEscogido
 
-let pulpas = [Pulpa1, Pulpa2, Pulpa3]
+cursoEscogido = prompt ("Escriba el número del curso que desea cursar 1. Inglés $2000, 2. Francés $5000 3.Italiano $8000 4. Mandarin $8000 5.Japones $8000")
 
-let seleccion = pulpas.find(pulpas => pulpas.sabor == 'fresa') 
-let filtro = pulpas.filter(pulpas => pulpas.sabor == 'maracuya')
+if (cursoEscogido !="") {
+    alert("Gracias por escogernos, continue con su compra")
 
-console.log(seleccion)
+}
+
+else{
+    alert("No debe haber campos vacíos")
+}
+
+function cargarUnItem()
+{
+const ingles = new Ingles('ingles', 2000)
+const frances = new Frances('frances', 5000)
+const italiano = new Italiano('italiano', 8000)
+const mandarin = new Italiano('mandarin', 8000)
+const japones = new Japones('japones', 8000)
+}
+
+function cargarItems(carrito)
+{
+    let articulo;
+    let seguir;
+    do{
+    articulo = cargarUnItem();
+    carrito.push(articulo);
+    seguir = prompt ("desea continuar comprando cursos? si/no")
+} while(seguir =='si')
+}
+
+function mostrarCarrito(carrito)
+{
+    for(let articulo of carrito)
+    {
+        console.log("Nombre: " + cursos.precio + "\n"
+                    + "Precio: " + cursos.sabor + "\n")
+}
+}
+
+//sumar precios del carrito
+total = carrito.reduce((suma, articulo) => {
+    return suma + articulo.precio
+}, 0)
+
+
+const carrito = []
+cargarItems(carrito);
+console.log(carrito);
+mostrarCarrito(carrito);
+
+
+
+
